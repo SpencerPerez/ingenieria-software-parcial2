291cb2c7796f2659692cc9730b10df808e9a1797
SpencerPerez <spereza2@miumg.edu.gt>
2019-04-24 20:00:56 -0500

    Add controller

--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
@@ -4,9 +4,7 @@ import gt.edu.umg.ingenieria.sistemas.core.parcial2.core.model.ProductoEntity;
 import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service.ProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/producto")
@@ -14,10 +12,29 @@ public class ProductoController {
 
     @Autowired
     private ProductoService productoService;
-    
+
     @GetMapping("/buscarTodos")
     public List<ProductoEntity> buscarTodos() {
         return this.productoService.buscarTodos();
     }
-    
+
+    @PostMapping("/registrarProducto")
+    public ProductoEntity create(@RequestBody ProductoEntity entity) {
+        entity.setName(
+                entity.getName().substring(0, 1).toUpperCase() + entity.getName().substring(1)
+        );
+        return this.productoService.registrarProducto(entity);
+    }
+
+    @PutMapping("/actualizarStock/{id}/incrementar/{amount}")
+    public String updateStockAdd(@PathVariable long id, @PathVariable int amount) {
+        ProductoEntity entity = this.productoService.incrementarStock(id, amount);
+        return "Stock actualizado: [" + entity.getName() + " " + entity.getStock() + "]";
+    }
+
+    @PutMapping("/actualizarStock/{id}/decrementar/{amount}")
+    public ProductoEntity updateStockSub(@PathVariable long id, @PathVariable int amount) {
+        return this.productoService.quitarStock(id, amount);
+    }
+
 }
