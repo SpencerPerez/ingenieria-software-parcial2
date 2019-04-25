99a4de0d42d28a135c7ceba1d08b9edde9e4d56e
SpencerPerez <spereza2@miumg.edu.gt>
2019-04-24 19:58:09 -0500

    add Service

--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
@@ -2,6 +2,9 @@ package gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service;
 
 import gt.edu.umg.ingenieria.sistemas.core.parcial2.core.model.ProductoEntity;
 import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.dao.ProductoRepository;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +14,26 @@ public class ProductoService {
 
     @Autowired
     private ProductoRepository productoRepository;
-    
+
     public List<ProductoEntity> buscarTodos() {
-        return (List<ProductoEntity>) this.productoRepository.findAll();
+    List<ProductoEntity> list = (List<ProductoEntity>) this.productoRepository.findAll();
+        list.sort(Comparator.comparing(ProductoEntity::getName));
+        return list;
     }
-    
+
+    public ProductoEntity registrarProducto(ProductoEntity productoEntity) {
+        return productoRepository.save(productoEntity);
+    }
+
+    public ProductoEntity incrementarStock(long id, int amount) {
+        ProductoEntity productoEntity = productoRepository.findById(id).get();
+        productoEntity.setStock(productoEntity.getStock() + amount);
+        return productoRepository.save(productoEntity);
+    }
+
+    public ProductoEntity quitarStock(long id, int amount) {
+        ProductoEntity productoEntity = productoRepository.findById(id).get();
+        productoEntity.setStock(productoEntity.getStock() - amount);
+        return productoRepository.save(productoEntity);
+    }
 }
