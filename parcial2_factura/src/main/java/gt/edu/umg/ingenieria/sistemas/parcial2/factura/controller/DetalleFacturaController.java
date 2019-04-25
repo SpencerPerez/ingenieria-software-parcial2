07b916145c1620d15d778cc6ecf6bd112e1da3e7
SpencerPerez <spereza2@miumg.edu.gt>
2019-04-24 20:28:00 -0500

    controller detalle de factura

--- parcial2_factura/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/factura/controller/DetalleFacturaController.java
+++ parcial2_factura/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/factura/controller/DetalleFacturaController.java
@@ -1,8 +1,21 @@
 package gt.edu.umg.ingenieria.sistemas.parcial2.factura.controller;
 
+import gt.edu.umg.ingenieria.sistemas.core.parcial2.core.model.DetalleFacturaEntity;
+import gt.edu.umg.ingenieria.sistemas.parcial2.factura.service.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DetalleFacturaController {
+    @Autowired
+    private FacturaService facturaService;
+
+    @PostMapping("/crearDetalleFactura/{id}")
+    public DetalleFacturaEntity create(@RequestBody DetalleFacturaEntity entity, @PathVariable long header) {
+        return this.facturaService.crearDetalleFactura(entity, header);
+    }
         
 }
